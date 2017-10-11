345f70aef5a59b3ada543dd0027cc11ec78279c6
Abraham Kinney <abraham.kinney@lootcrate.com>
2017-10-10 21:08:28 -0700

    axios installed and hitting rails API

--- src/CommentsContainer.js
+++ src/CommentsContainer.js
@@ -1,6 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class CommentsContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      comments: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/comments.json')
+    .then(response => {
+      console.log(response)
+      this.setState({comments: response.data})
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div>
